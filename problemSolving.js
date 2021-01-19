51dd169bc0e2893b236d8275465a4a796d29827a
Mohammad Abdur Rahman <rahman.just16@gmail.com>
2021-01-19 12:45:22 +0600

    first commit

--- problemSolving.js
+++ problemSolving.js
@@ -1,12 +1,29 @@
 
 //factorial using while loop
 
-let i=1;
-let fact=1;
-while(i<=10){
-    fact= fact*i;
-    // console.log(i,fact);
-    i++;
+// let i=1;
+// let fact=1;
+// while(i<=10){
+//     fact= fact*i;
+//     // console.log(i,fact);
+//     i++;
     
+// }
+// console.log(fact);
+
+
+//recursive factorial using function
+
+
+function factorial(n){
+    if (n==0){
+        return 1;
+    }
+    else{
+        return n*factorial(n-1);
+    }
+
 }
-console.log(fact);
+console.log(factorial(4));
+
+
