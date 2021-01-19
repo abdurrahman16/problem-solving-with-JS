13d3ac84e5e65a8cd1f16f46dd7c79d22464edec
Mohammad Abdur Rahman <rahman.just16@gmail.com>
2021-01-19 13:01:37 +0600

    first commit

--- problemSolving.js
+++ problemSolving.js
@@ -15,15 +15,33 @@
 //recursive factorial using function
 
 
-function factorial(n){
-    if (n==0){
-        return 1;
-    }
-    else{
-        return n*factorial(n-1);
-    }
+// function factorial(n){
+//     if (n==0){
+//         return 1;
+//     }
+//     else{
+//         return n*factorial(n-1);
+//     }
 
+// }
+// console.log(factorial(4));
+
+// fiboncacci with JS
+
+
+
+
+function fibonacci(n){
+    let fibo= [0,1];
+    for (let i=2; i<=n; i++)
+{
+    fibo[i]= fibo[i-1]+ fibo[i-2];
+    // console.log(fibo);
+    
 }
-console.log(factorial(4));
+return fibo;
+}
+console.log(fibonacci(12));
+
 
 
